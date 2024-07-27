5a004cad03f8cfeaa43f9f34d0f2dd32671b3b29
Shivam77607 <shivamsharma77607@gmail.com>
2024-07-27 08:02:04 +0530

    refactor: Improve build server script logging and error handling

--- build-server/script.js
+++ build-server/script.js
@@ -3,6 +3,9 @@ const path = require("path");
 const fs = require("fs");
 const { S3, PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
+const Redis = require("ioredis");
+
+const publisher = new Redis("");
 
 const s3Client = new S3Client({
   region: "",
@@ -14,18 +17,25 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
+function publishLog({ log }) {
+  publisher.publish(`log:${PROJECT_ID}`, JSON.stringify({ log }));
+}
+
 async function init() {
   console.log("Executing script.js");
+  publishLog("Build started");
   const outDirPath = `${__dirname}/output`;
 
   const p = exec(`cd ${outDirPath} && npm install && npm run build`);
 
   p.stdout.on("data", (data) => {
     console.log(data.toString());
+    publishLog(data.toString());
   });
 
   p.stdout.on("error", (data) => {
     console.log("Error: ", data.toString());
+    publishLog("Error: ", data.toString());
   });
 
   p.on("close", async () => {
@@ -35,12 +45,15 @@ async function init() {
       recursive: true,
     });
 
+    publishLog("Starting to upload files");
+
     for (const file of distFolderContents) {
       const filePath = `${distFolderPath}/${file}`;
 
       if (fs.lstatSync(filePath).isDirectory()) continue;
 
       console.log("Uploading: ", filePath);
+      publishLog("Uploading: ", file);
 
       const command = new PutObjectCommand({
         Bucket: "instantdeploy",
@@ -52,8 +65,10 @@ async function init() {
       await s3Client.send(command);
 
       console.log("Uploaded: ", filePath);
+      publishLog("Uploaded: ", file);
     }
     console.log("Done uploading files");
+    publishLog("Done uploading files");
   });
 }
 
